cdf3f9f40f9c6924ae4f8c9664ee55d9dcada15b
judy (Novell) <judy@novell.openldap.org>
2000-08-25 19:38:45 +0000

    Edited Javadoc.

--- com/novell/ldap/LDAPBind.java
+++ com/novell/ldap/LDAPBind.java
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * $Novell: LDAPBind.java,v 1.2 2000/03/14 18:17:25 smerrill Exp $
+ * $Novell: /ldap/src/jldap/ldap/src/org/ietf/ldap/LDAPBind.java,v 1.3 2000/08/03 22:06:13 smerrill Exp $
  *
  * Copyright (C) 1999, 2000 Novell, Inc. All Rights Reserved.
  * 
@@ -14,14 +14,19 @@
  ***************************************************************************/
  
 package org.ietf.ldap;
+
+/*
+* 4.4 public interface LDAPBind
+*/
  
 /**
- * 4.4 public interface LDAPBind
  *
- *  Used to do explicit bind processing on a referral. A client may
- *  specify an instance of this class to be used on a single operation
- *  (through the LDAPConstraints object) or for all operations (through
- *  LDAPConnection.setOption()).
+ *  Used to do explicit bind processing on a referral. 
+ *
+ *  <p>A client can specify an instance of this class to be used
+ *  on a single operation (through the LDAPConstraints object) 
+ *  or for all operations (through LDAPConnection.setOption method).</p>
+ *  
  */
 public interface LDAPBind {
 
@@ -30,14 +35,18 @@ public interface LDAPBind {
     */
 
    /**
-    * This method is called by LDAPConnection when authenticating. An
-    * implementation may access the host, port, credentials, and other
-    * information in the LDAPConnection to decide on an appropriate
-    * authentication mechanism, and/or may interact with a user or external
-    * module. An LDAPException is thrown on failure, as in
-    * LDAPConnection.bind().
+    * Called by LDAPConnection when authenticating.
     *
-    * conn           An established connection to an LDAP server.
+    * <p> An implementation can access the host, port, credentials, and other
+    * information in the LDAPConnection to decide on an appropriate
+    * authentication mechanism, and/or interact with a user or external
+    * module. An LDAPException is thrown on failure, as in
+    * LDAPConnection.bind method. </p>
+    *
+    * @param conn    An established connection to an LDAP server.
+    *
+    * @exception  LDAPException A general exception which includes an error
+    * message and an LDAP error code.
     */
    public void bind (LDAPConnection conn) throws LDAPException;
 
