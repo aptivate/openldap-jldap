abb0a66121d2df1cd6162e81c26d6fc63be94c2d
smerrill (Novell) <smerrill@novell.openldap.org>
2000-09-02 23:09:23 +0000

    Changed to use LBEREncoder instead of BEREncoder.

--- com/novell/ldap/client/Connection.java
+++ com/novell/ldap/client/Connection.java
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * $Novell: /ldap/src/jldap/ldap/src/com/novell/ldap/client/Connection.java,v 1.11 2000/08/22 01:47:26 smerrill Exp $
+ * $Novell: /ldap/src/jldap/ldap/src/com/novell/ldap/client/Connection.java,v 1.12 2000/08/28 22:19:00 vtag Exp $
  *
  * Copyright (C) 1999, 2000 Novell, Inc. All Rights Reserved.
  * 
@@ -30,32 +30,32 @@ import com.novell.asn1.ldap.UnbindRequest;
   */
 public final class Connection implements Runnable {
 
-	// The listener thread will multiplex response messages received from the
-	// server to one of many queues. Each LDAPListener which registers with
-	// this class will have its own message queue. That message queue may be
-	// dedicated to a single LDAP operation, or may be shared among many LDAP
-	// operations.
-	//
-	// The applications thread, using an LDAPListener, writes data directly
-	// to the server using this class. The application thread will then query
-	// the LDAPListener for a response.
-	//
-	// The listener thread reads data directly from the server as it decodes
-	// an LDAPMessage and writes it to a message queue associated with either
-	// an LDAPResponseListener, or an LDAPSearchListener. It uses the message
-	// ID from the response to determine which listener is expecting the
-	// result. It does this by getting a list of message ID's from each
-	// listener, and comparing the message ID from the message just received
-	// and adding the message to that listeners queue.
-	//
-	// Note: the listener thread must not be a "selfish" thread, since some
-	// operating systems do not time slice.
-	//
+   // The listener thread will multiplex response messages received from the
+   // server to one of many queues. Each LDAPListener which registers with
+   // this class will have its own message queue. That message queue may be
+   // dedicated to a single LDAP operation, or may be shared among many LDAP
+   // operations.
+   //
+   // The applications thread, using an LDAPListener, writes data directly
+   // to the server using this class. The application thread will then query
+   // the LDAPListener for a response.
+   //
+   // The listener thread reads data directly from the server as it decodes
+   // an LDAPMessage and writes it to a message queue associated with either
+   // an LDAPResponseListener, or an LDAPSearchListener. It uses the message
+   // ID from the response to determine which listener is expecting the
+   // result. It does this by getting a list of message ID's from each
+   // listener, and comparing the message ID from the message just received
+   // and adding the message to that listeners queue.
+   //
+   // Note: the listener thread must not be a "selfish" thread, since some
+   // operating systems do not time slice.
+   //
    private Thread listener; // New thread that reads data from the server.
    private boolean v3 = true;
 
-	private BEREncoder encoder = new BEREncoder();
-	private BERDecoder decoder = new BERDecoder();
+   private LBEREncoder encoder = new LBEREncoder();
+   private LBERDecoder decoder = new LBERDecoder();
 
    private String host;
    private int port;
@@ -66,7 +66,7 @@ public final class Connection implements Runnable {
    private OutputStream out;
    private Socket socket;
 
-	private Vector ldapListeners;
+   private Vector ldapListeners;
 
    // true means v3; false means v2
    void setV3(boolean v) {
@@ -98,21 +98,21 @@ public final class Connection implements Runnable {
       return out;
    }
 
-	public String getHost() {
-		return host;
-	}
+   public String getHost() {
+      return host;
+   }
 
-	public int getPort() {
-		return port;
-	}
+   public int getPort() {
+      return port;
+   }
 
    /**
     * Constructs a TCP/IP connection to a server specified in host and port.
-	 * The socketFactory parameter produces SSL sockets.
+    * The socketFactory parameter produces SSL sockets.
     */
    public Connection(String host, int port, LDAPSocketFactory socketFactory)
       throws LDAPException
-	{
+   {
       this.host = host;
       this.port = port;
 
@@ -133,98 +133,98 @@ public final class Connection implements Runnable {
                                  LDAPException.CONNECT_ERROR);
       }
 
-		ldapListeners = new Vector(5);
+      ldapListeners = new Vector(5);
 
       listener = new Thread(this);
       listener.setDaemon(true); // If this is the last thread running, exit.
       listener.start();
    }
 
-	/**
-	 * Writes an LDAPMessage to the LDAP server over a socket.
-	 */
+   /**
+    * Writes an LDAPMessage to the LDAP server over a socket.
+    */
    public void writeMessage(com.novell.ldap.LDAPMessage msg)
-		throws IOException
-	{
-		byte[] ber = msg.getASN1Object().getEncoding(encoder);
+      throws IOException
+   {
+      byte[] ber = msg.getASN1Object().getEncoding(encoder);
       synchronized(this) {
          out.write(ber, 0, ber.length);
          out.flush();
       }
    }
 
-	//------------------------------------------------------------------------
-	// Methods to manage the LDAP Listeners
-	//------------------------------------------------------------------------
+   //------------------------------------------------------------------------
+   // Methods to manage the LDAP Listeners
+   //------------------------------------------------------------------------
 
-	/**
-	 *
-	 */
-	public void addLDAPListener(LDAPListener listener) {
-		ldapListeners.addElement(listener);
-	}
+   /**
+    *
+    */
+   public void addLDAPListener(LDAPListener listener) {
+      ldapListeners.addElement(listener);
+   }
 
-	/**
-	 *
-	 */
-	public void removeLDAPListener(LDAPListener listener) {
-		ldapListeners.remove(listener);
-	}
+   /**
+    *
+    */
+   public void removeLDAPListener(LDAPListener listener) {
+      ldapListeners.remove(listener);
+   }
 
-	/**
-	 *
-	 */
-	public void abandon(int msgId) {
-		// Find the message queue which owns this request.
-		int cnt = ldapListeners.size();
-		findMsgId:
-			for(int i=0; i<cnt; i++) {
-				LDAPListener ldapListener =
-					 (LDAPListener)ldapListeners.elementAt(i);
-				int[] msgIDs = ldapListener.getMessageIDs();
-				for(int j=0; j<msgIDs.length; j++)
-					if(msgIDs[j] == msgId) {
-						ldapListener.removeResponses(msgId); // responses already received
-						ldapListener.removeMessageIDAndNotify(msgId); // notifies
-						break findMsgId; // we're done, so bail out
-					}
-			}
-	}
+   /**
+    *
+    */
+   public void abandon(int msgId) {
+      // Find the message queue which owns this request.
+      int cnt = ldapListeners.size();
+      findMsgId:
+         for(int i=0; i<cnt; i++) {
+            LDAPListener ldapListener =
+                (LDAPListener)ldapListeners.elementAt(i);
+            int[] msgIDs = ldapListener.getMessageIDs();
+            for(int j=0; j<msgIDs.length; j++)
+               if(msgIDs[j] == msgId) {
+                  ldapListener.removeResponses(msgId); // responses already received
+                  ldapListener.removeMessageIDAndNotify(msgId); // notifies
+                  break findMsgId; // we're done, so bail out
+               }
+         }
+   }
 
-	/**
-	 *
-	 */
+   /**
+    *
+    */
    protected void finalize() {
       shutdown(null);
    }
 
-	/**
-	 * This method may be called by finalize() for the connection, or it may
-	 * be called by LDAPConnection.disconnect().
-	 */
+   /**
+    * This method may be called by finalize() for the connection, or it may
+    * be called by LDAPConnection.disconnect().
+    */
    public synchronized void shutdown(LDAPControl[] reqCtls) {
 
       if(socket != null) {
          try {
             // abandonOutstandingReqs(reqCtls);
             if(bound) {
-					writeMessage(new LDAPMessage(new UnbindRequest(), reqCtls));
+               writeMessage(new LDAPMessage(new UnbindRequest(), reqCtls));
             }
          }
-			catch(IOException ioe) {
+         catch(IOException ioe) {
 
-			}
+         }
          finally {
             try {
                out.flush();
                socket.close();
             }
             catch(java.io.IOException ie) {
-					// problem closing socket
+               // problem closing socket
             }
 
-				// make sure no thread is left doing a wait()!!!
-				// notify each object that may be waiting....
+            // make sure no thread is left doing a wait()!!!
+            // notify each object that may be waiting....
 
             socket = null;
          } 
@@ -232,56 +232,56 @@ public final class Connection implements Runnable {
    }
 
 
-	/**
+   /**
     * The thread that decodes and processes LDAPMessage's from the server.
-	 *
-	 * Note: This thread needs a graceful shutdown implementation.
-	 */
+    *
+    * Note: This thread needs a graceful shutdown implementation.
+    */
    public void run() {
       try {
-			for(;;) {
-				// ------------------------------------------------------------
-				// Decode an LDAPMessage directly from the socket.
-				// ------------------------------------------------------------
-				ASN1Identifier asn1ID = new ASN1Identifier(in);
-				if(asn1ID.getTag() != ASN1Structured.SEQUENCE)
-					continue; // loop looking for an LDAPMessage identifier
+         for(;;) {
+            // ------------------------------------------------------------
+            // Decode an LDAPMessage directly from the socket.
+            // ------------------------------------------------------------
+            ASN1Identifier asn1ID = new ASN1Identifier(in);
+            if(asn1ID.getTag() != ASN1Structured.SEQUENCE)
+               continue; // loop looking for an LDAPMessage identifier
 
-				ASN1Length asn1Len = new ASN1Length(in);
+            ASN1Length asn1Len = new ASN1Length(in);
 
-				com.novell.asn1.ldap.LDAPMessage msg =
-					 new com.novell.asn1.ldap.LDAPMessage(
-						 decoder, in, asn1Len.getLength());
+            com.novell.asn1.ldap.LDAPMessage msg =
+                new com.novell.asn1.ldap.LDAPMessage(
+                   decoder, in, asn1Len.getLength());
 
-				// ------------------------------------------------------------
-				// Process the decoded LDAPMessage.
-				// ------------------------------------------------------------
-				int msgId = msg.getMessageID();
+            // ------------------------------------------------------------
+            // Process the decoded LDAPMessage.
+            // ------------------------------------------------------------
+            int msgId = msg.getMessageID();
 
-				if(msgId == 0) {
-					// Process Unsolicited Notification
-				}
-				else {
-					// Find the message queue which requested this response.
-					// It is possible to receive a response for a request which
-					// has been abandoned. If abandoned, do nothing.
-					int cnt = ldapListeners.size();
-					findMsgId:
-						for(int i=0; i<cnt; i++) {
-							LDAPListener ldapListener =
-								 (LDAPListener)ldapListeners.elementAt(i);
-							int[] msgIDs = ldapListener.getMessageIDs();
-							for(int j=0; j<msgIDs.length; j++)
-								if(msgIDs[j] == msgId) {
-									ldapListener.addLDAPMessage(msg); //notifies
-									break findMsgId; // we're done, so bail out
-								}
-						}
-				}
+            if(msgId == 0) {
+               // Process Unsolicited Notification
+            }
+            else {
+               // Find the message queue which requested this response.
+               // It is possible to receive a response for a request which
+               // has been abandoned. If abandoned, do nothing.
+               int cnt = ldapListeners.size();
+               findMsgId:
+                  for(int i=0; i<cnt; i++) {
+                     LDAPListener ldapListener =
+                         (LDAPListener)ldapListeners.elementAt(i);
+                     int[] msgIDs = ldapListener.getMessageIDs();
+                     for(int j=0; j<msgIDs.length; j++)
+                        if(msgIDs[j] == msgId) {
+                           ldapListener.addLDAPMessage(msg); //notifies
+                           break findMsgId; // we're done, so bail out
+                        }
+                  }
+            }
          }
       }
       catch(IOException ioe) {
-//			ioe.printStackTrace();
+//       ioe.printStackTrace();
       }
       finally {
          shutdown(null);
