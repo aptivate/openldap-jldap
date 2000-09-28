9e898b1c60494405f4c0973360ba646a103ec784
Steve Sonntag <vtag@openldap.org>
2000-09-28 17:17:41 +0000

    Fix tabs

--- com/novell/ldap/LDAPResponse.java
+++ com/novell/ldap/LDAPResponse.java
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * $Novell: /ldap/src/jldap/com/novell/ldap/LDAPResponse.java,v 1.11 2000/09/13 16:15:40 vtag Exp $
+ * $Novell: /ldap/src/jldap/com/novell/ldap/LDAPResponse.java,v 1.12 2000/09/21 23:27:35 vtag Exp $
  *
  * Copyright (C) 1999, 2000 Novell, Inc. All Rights Reserved.
  * 
@@ -25,154 +25,155 @@ import com.novell.ldap.client.Debug;
 /**
  *  Represents the response to an LDAP protocol operation.
  */
-public class LDAPResponse extends LDAPMessage {
+public class LDAPResponse extends LDAPMessage
+{
 
-	/**
-	 * Creates an LDAPMessage when receiving an RFC 2251 LDAPMessage from a
-	 * server.
+    /**
+     * Creates an LDAPMessage when receiving an RFC 2251 LDAPMessage from a
+     * server.
      *
      *  @param message  The LDAPMessage from a server.
-	 */
-	public LDAPResponse(com.novell.ldap.protocol.LDAPMessage message)
-	{
-		super(message);
-	}
+     */
+    public LDAPResponse(com.novell.ldap.protocol.LDAPMessage message)
+    {
+        super(message);
+    }
 
-   /**
-    * Returns any error message in the response.
-    *
-    * @return Any error message in the response.
-    */
-   public String getErrorMessage()
-	{
-		return
-			((Response)message.getProtocolOp()).getErrorMessage().getString();
+    /**
+     * Returns any error message in the response.
+     *
+     * @return Any error message in the response.
+     */
+    public String getErrorMessage()
+    {
+        return
+            ((Response)message.getProtocolOp()).getErrorMessage().getString();
+    }
+
+    /**
+     * Returns the partially matched DN field, if any, in a server response.
+     *
+     * @return The partially matched DN field, if the response contains one.
+     *         
+     */
+    public String getMatchedDN()
+    {
+        return ((Response)message.getProtocolOp()).getMatchedDN().getString();
+    }
+
+    /**
+     * Returns all referrals, if any, in a server response.
+     *
+     * @return All the referrals in the server response.
+     */
+    public String[] getReferrals()
+    {
+        String[] referrals = null;
+        Referral ref = ((Response)message.getProtocolOp()).getReferral();
+        
+        if(ref != null) {
+            // convert RFC 2251 Referral to String[]
+            int size = ref.size();
+            referrals = new String[size];
+            for(int i=0; i<size; i++) {
+                referrals[i] = new String(((ASN1OctetString)ref.get(i)).getContent());
+            }
+        }
+
+        return referrals;
    }
 
-   /**
-    * Returns the partially matched DN field, if any, in a server response.
-    *
-    * @return The partially matched DN field, if the response contains one.
-    *         
-    */
-   public String getMatchedDN()
-	{
-		return
-			((Response)message.getProtocolOp()).getMatchedDN().getString();
-   }
+    /**
+     * Returns the result code in a server response.
+     *
+     * <p> For a list of result codes, see the LDAPException class. </p>
+     *
+     * @return The result code.
+     */
+    public int getResultCode()
+    {
+        return ((Response)message.getProtocolOp()).getResultCode().getInt();
+    }
 
-   /**
-    * Returns all referrals, if any, in a server response.
-    *
-    * @return All the referrals in the server response.
-    */
-   public String[] getReferrals() {
-		String[] referrals = null;
-		Referral ref = ((Response)message.getProtocolOp()).getReferral();
-		
-		if(ref != null) {
-			// convert RFC 2251 Referral to String[]
-			int size = ref.size();
-			referrals = new String[size];
-			for(int i=0; i<size; i++) {
-				referrals[i] = new String(((ASN1OctetString)ref.get(i)).getContent());
-			}
-		}
-
-      return referrals;
-   }
-
-   /**
-    * Returns the result code in a server response.
-    *
-    * <p> For a list of result codes, see the LDAPException class. </p>
-    *
-    * @return The result code.
-    */
-   public int getResultCode()
-	{
-		return ((Response)message.getProtocolOp()).getResultCode().getInt();
-   }
-
-   /**
-    * Checks the resultCode and throws the appropriate exception.
-    */
-   /* package */ void chkResultCode() throws LDAPException {
-      switch(getResultCode()) {
+    /**
+     * Checks the resultCode and throws the appropriate exception.
+     */
+    /* package */
+    void chkResultCode() throws LDAPException
+    {
+        switch(getResultCode()) {
         case LDAPException.SUCCESS:
-        	break;
+            break;
         case LDAPException.OPERATIONS_ERROR:
         case LDAPException.PROTOCOL_ERROR:
         case LDAPException.TIME_LIMIT_EXCEEDED:
-		case LDAPException.SIZE_LIMIT_EXCEEDED:
-		case LDAPException.AUTH_METHOD_NOT_SUPPORTED:
-		case LDAPException.STRONG_AUTH_REQUIRED:
-		case LDAPException.LDAP_PARTIAL_RESULTS:
-		case LDAPException.ADMIN_LIMIT_EXCEEDED:
-		case LDAPException.UNAVAILABLE_CRITICAL_EXTENSION:
-		case LDAPException.CONFIDENTIALITY_REQUIRED:
-		case LDAPException.SASL_BIND_IN_PROGRESS:
-		case LDAPException.NO_SUCH_ATTRIBUTE:
-		case LDAPException.UNDEFINED_ATTRIBUTE_TYPE:
-		case LDAPException.INAPPROPRIATE_MATCHING:
-		case LDAPException.CONSTRAINT_VIOLATION:
-		case LDAPException.ATTRIBUTE_OR_VALUE_EXISTS:
-		case LDAPException.INVALID_ATTRIBUTE_SYNTAX:
-		case LDAPException.NO_SUCH_OBJECT:
-		case LDAPException.ALIAS_PROBLEM:
-		case LDAPException.INVALID_DN_SYNTAX:
-		case LDAPException.IS_LEAF:
-		case LDAPException.ALIAS_DEREFERENCING_PROBLEM:
-		case LDAPException.INAPPROPRIATE_AUTHENTICATION:
-		case LDAPException.INVALID_CREDENTIALS:
-		case LDAPException.INSUFFICIENT_ACCESS_RIGHTS:
-		case LDAPException.BUSY:
-		case LDAPException.UNAVAILABLE:
-		case LDAPException.UNWILLING_TO_PERFORM:
-		case LDAPException.LOOP_DETECT:
-		case LDAPException.NAMING_VIOLATION:
-		case LDAPException.OBJECT_CLASS_VIOLATION:
-		case LDAPException.NOT_ALLOWED_ON_NONLEAF:
-		case LDAPException.NOT_ALLOWED_ON_RDN:
-		case LDAPException.ENTRY_ALREADY_EXISTS:
-		case LDAPException.OBJECT_CLASS_MODS_PROHIBITED:
-		case LDAPException.AFFECTS_MULTIPLE_DSAS:
-		case LDAPException.OTHER:
-		case LDAPException.SERVER_DOWN:
-		case LDAPException.LOCAL_ERROR:
-		case LDAPException.ENCODING_ERROR:
-		case LDAPException.DECODING_ERROR:
-		case LDAPException.LDAP_TIMEOUT:
-		case LDAPException.AUTH_UNKNOWN:
-		case LDAPException.FILTER_ERROR:
-		case LDAPException.USER_CANCELLED:
-		case LDAPException.PARAM_ERROR:
-		case LDAPException.NO_MEMORY:
-		case LDAPException.CONNECT_ERROR:
-		case LDAPException.LDAP_NOT_SUPPORTED:
-		case LDAPException.CONTROL_NOT_FOUND:
-		case LDAPException.NO_RESULTS_RETURNED:
-		case LDAPException.MORE_RESULTS_TO_RETURN:
-		case LDAPException.CLIENT_LOOP:
-		case LDAPException.REFERRAL_LIMIT_EXCEEDED:
-			throw new LDAPException(getErrorMessage(), getMatchedDN(),
-				getResultCode());
+        case LDAPException.SIZE_LIMIT_EXCEEDED:
+        case LDAPException.AUTH_METHOD_NOT_SUPPORTED:
+        case LDAPException.STRONG_AUTH_REQUIRED:
+        case LDAPException.LDAP_PARTIAL_RESULTS:
+        case LDAPException.ADMIN_LIMIT_EXCEEDED:
+        case LDAPException.UNAVAILABLE_CRITICAL_EXTENSION:
+        case LDAPException.CONFIDENTIALITY_REQUIRED:
+        case LDAPException.SASL_BIND_IN_PROGRESS:
+        case LDAPException.NO_SUCH_ATTRIBUTE:
+        case LDAPException.UNDEFINED_ATTRIBUTE_TYPE:
+        case LDAPException.INAPPROPRIATE_MATCHING:
+        case LDAPException.CONSTRAINT_VIOLATION:
+        case LDAPException.ATTRIBUTE_OR_VALUE_EXISTS:
+        case LDAPException.INVALID_ATTRIBUTE_SYNTAX:
+        case LDAPException.NO_SUCH_OBJECT:
+        case LDAPException.ALIAS_PROBLEM:
+        case LDAPException.INVALID_DN_SYNTAX:
+        case LDAPException.IS_LEAF:
+        case LDAPException.ALIAS_DEREFERENCING_PROBLEM:
+        case LDAPException.INAPPROPRIATE_AUTHENTICATION:
+        case LDAPException.INVALID_CREDENTIALS:
+        case LDAPException.INSUFFICIENT_ACCESS_RIGHTS:
+        case LDAPException.BUSY:
+        case LDAPException.UNAVAILABLE:
+        case LDAPException.UNWILLING_TO_PERFORM:
+        case LDAPException.LOOP_DETECT:
+        case LDAPException.NAMING_VIOLATION:
+        case LDAPException.OBJECT_CLASS_VIOLATION:
+        case LDAPException.NOT_ALLOWED_ON_NONLEAF:
+        case LDAPException.NOT_ALLOWED_ON_RDN:
+        case LDAPException.ENTRY_ALREADY_EXISTS:
+        case LDAPException.OBJECT_CLASS_MODS_PROHIBITED:
+        case LDAPException.AFFECTS_MULTIPLE_DSAS:
+        case LDAPException.OTHER:
+        case LDAPException.SERVER_DOWN:
+        case LDAPException.LOCAL_ERROR:
+        case LDAPException.ENCODING_ERROR:
+        case LDAPException.DECODING_ERROR:
+        case LDAPException.LDAP_TIMEOUT:
+        case LDAPException.AUTH_UNKNOWN:
+        case LDAPException.FILTER_ERROR:
+        case LDAPException.USER_CANCELLED:
+        case LDAPException.PARAM_ERROR:
+        case LDAPException.NO_MEMORY:
+        case LDAPException.CONNECT_ERROR:
+        case LDAPException.LDAP_NOT_SUPPORTED:
+        case LDAPException.CONTROL_NOT_FOUND:
+        case LDAPException.NO_RESULTS_RETURNED:
+        case LDAPException.MORE_RESULTS_TO_RETURN:
+        case LDAPException.CLIENT_LOOP:
+        case LDAPException.REFERRAL_LIMIT_EXCEEDED:
+            throw new LDAPException(getErrorMessage(), getMatchedDN(),
+                getResultCode());
             case LDAPException.REFERRAL:
-	            // only throw this if automatic referral handling has not been
-	            // enabled.
-				String[] refs = getReferrals();
-				if( Debug.LDAP_DEBUG ) {
-					if( Debug.trace( "referrals" )) {
-						Debug.trace( "referrals", "LDAPResponse: Generating Referral Exception");
-						for( int i = 0; i < refs.length; i++) {
-							Debug.trace( "referrals", "LDAPResponse: \t" + refs[i]);
-						}
-					}
-				}
-	            throw new LDAPReferralException(getErrorMessage(), LDAPException.REFERRAL, refs);
+                // only throw this if automatic referral handling has not been
+                // enabled.
+                String[] refs = getReferrals();
+                if( Debug.LDAP_DEBUG ) {
+                    Debug.trace( Debug.messages, "LDAPResponse: Generating Referral Exception");
+                    for( int i = 0; i < refs.length; i++) {
+                        Debug.trace( Debug.messages, "LDAPResponse: \t" + refs[i]);
+                    }
+                }
+                throw new LDAPReferralException(getErrorMessage(), LDAPException.REFERRAL, refs);
             default: // unknown
-	            throw new LDAPException(getErrorMessage(), getMatchedDN(),
-            		getResultCode());
-		}
-	}
+                throw new LDAPException(getErrorMessage(), getMatchedDN(),
+                    getResultCode());
+        }
+    }
 }
--- com/novell/ldap/LDAPSearchResults.java
+++ com/novell/ldap/LDAPSearchResults.java
@@ -1,18 +1,18 @@
 /* **************************************************************************
- * $Novell: /ldap/src/jldap/com/novell/ldap/LDAPSearchResults.java,v 1.10 2000/09/14 15:29:30 judy Exp $
- *
- * Copyright (C) 1999, 2000 Novell, Inc. All Rights Reserved.
- * 
- * THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
- * TREATIES. USE, MODIFICATION, AND REDISTRIBUTION OF THIS WORK IS SUBJECT
- * TO VERSION 2.0.1 OF THE OPENLDAP PUBLIC LICENSE, A COPY OF WHICH IS
- * AVAILABLE AT HTTP://WWW.OPENLDAP.ORG/LICENSE.HTML OR IN THE FILE "LICENSE"
- * IN THE TOP-LEVEL DIRECTORY OF THE DISTRIBUTION. ANY USE OR EXPLOITATION
- * OF THIS WORK OTHER THAN AS AUTHORIZED IN VERSION 2.0.1 OF THE OPENLDAP
- * PUBLIC LICENSE, OR OTHER PRIOR WRITTEN CONSENT FROM NOVELL, COULD SUBJECT
- * THE PERPETRATOR TO CRIMINAL AND CIVIL LIABILITY. 
- ***************************************************************************/
- 
+* $Novell: /ldap/src/jldap/com/novell/ldap/LDAPSearchResults.java,v 1.11 2000/09/27 22:44:52 vtag Exp $
+*
+* Copyright (C) 1999, 2000 Novell, Inc. All Rights Reserved.
+* 
+* THIS WORK IS SUBJECT TO U.S. AND INTERNATIONAL COPYRIGHT LAWS AND
+* TREATIES. USE, MODIFICATION, AND REDISTRIBUTION OF THIS WORK IS SUBJECT
+* TO VERSION 2.0.1 OF THE OPENLDAP PUBLIC LICENSE, A COPY OF WHICH IS
+* AVAILABLE AT HTTP://WWW.OPENLDAP.ORG/LICENSE.HTML OR IN THE FILE "LICENSE"
+* IN THE TOP-LEVEL DIRECTORY OF THE DISTRIBUTION. ANY USE OR EXPLOITATION
+* OF THIS WORK OTHER THAN AS AUTHORIZED IN VERSION 2.0.1 OF THE OPENLDAP
+* PUBLIC LICENSE, OR OTHER PRIOR WRITTEN CONSENT FROM NOVELL, COULD SUBJECT
+* THE PERPETRATOR TO CRIMINAL AND CIVIL LIABILITY. 
+***************************************************************************/
+
 package com.novell.ldap;
 
 import com.novell.ldap.client.*;
@@ -20,45 +20,46 @@ import java.util.*;
 import java.io.*;
 
 /*
- * 4.35 public class LDAPSearchResults
- */
- 
+* 4.35 public class LDAPSearchResults
+*/
+
 /**
- *
- *  Implements an enumeration, thereby providing access to all entries
- *  retrieved during the synchronous search operation.
- */
-public class LDAPSearchResults implements Enumeration {
+*
+*  Implements an enumeration, thereby providing access to all entries
+*  retrieved during the synchronous search operation.
+*/
+public class LDAPSearchResults implements Enumeration
+{
 
-   private Vector entries;
-   private Enumeration elements;
-   private int batchSize;
-   private boolean completed = false;
-   private int count = 0;
-   private LDAPControl[] controls;
-   private LDAPSearchListener listener;
+    private Vector entries;
+    private Enumeration elements;
+    private int batchSize;
+    private boolean completed = false;
+    private int count = 0;
+    private LDAPControl[] controls;
+    private LDAPSearchListener listener;
 
-  /**
-   * Constructs a listener object for search results.
-   *
-   * @param batchSize The maximum number of messages for the listener's queue.
-   *<br><br>
-   * @param listener The listener for the search results.
-   */
-   public LDAPSearchResults(int batchSize, LDAPSearchListener listener)
-   {
-      this.listener = listener;
-      entries = new Vector((batchSize == 0) ? 64 : batchSize);
-      this.batchSize = (batchSize == 0) ? Integer.MAX_VALUE : batchSize;
-      completed = getBatchOfResults(); // initialize the enumeration
-      elements = entries.elements();
-   }
+    /**
+    * Constructs a listener object for search results.
+    *
+    * @param batchSize The maximum number of messages for the listener's queue.
+    *<br><br>
+    * @param listener The listener for the search results.
+    */
+    public LDAPSearchResults(int batchSize, LDAPSearchListener listener)
+    {
+        this.listener = listener;
+        entries = new Vector((batchSize == 0) ? 64 : batchSize);
+        this.batchSize = (batchSize == 0) ? Integer.MAX_VALUE : batchSize;
+        completed = getBatchOfResults(); // initialize the enumeration
+        elements = entries.elements();
+    }
 
-   /*
+    /*
     * 4.35.1 getCount
     */
 
-   /**
+    /**
     * Returns a count of the entries in the search result. 
     *
     * <p>If the search is asynchronous (batch size not 0),
@@ -66,16 +67,17 @@ public class LDAPSearchResults implements Enumeration {
     *
     * @return The number of search results received so far.  
     */
-   public int getCount() {
-      // when referrals are chased, they need to be added here!!!
-      return count;
-   }
+    public int getCount()
+    {
+        // when referrals are chased, they need to be added here!!!
+        return count;
+    }
 
-   /*
+    /*
     * 4.35.2 getResponseControls
     */
 
-   /**
+    /**
     * Returns the latest server controls returned by a directory server
     * in the context of this search request, or null
     * if no server controls were returned.
@@ -83,37 +85,39 @@ public class LDAPSearchResults implements Enumeration {
     * @return The server controls returned with the search request, or null
     *         if none were returned.
     */
-   public LDAPControl[] getResponseControls() {
-      return controls;
-   }
+    public LDAPControl[] getResponseControls()
+    {
+        return controls;
+    }
 
-   /*
+    /*
     * 4.35.3 hasMoreElements
     */
 
-   /**
+    /**
     * Specifies whether or not there are more search results in the
     * enumeration. 
     *
     * @return True, if there are more search results; false, if there are none.
     */
-   public boolean hasMoreElements() {
-      if(elements.hasMoreElements() == true)
-         return true;
-      if(completed == false) { // reload the enumeration
-         entries.setSize(0);
-         completed = getBatchOfResults();
-         elements = entries.elements();
-         return elements.hasMoreElements();
-      }
-      return false;
-   }
+    public boolean hasMoreElements()
+    {
+        if(elements.hasMoreElements() == true)
+            return true;
+        if(completed == false) { // reload the enumeration
+            entries.setSize(0);
+            completed = getBatchOfResults();
+            elements = entries.elements();
+            return elements.hasMoreElements();
+        }
+    return false;
+    }
 
-   /*
+    /*
     * 4.35.4 next
     */
 
-   /**
+    /**
     * Returns the next result in the enumeration as an LDAPEntry. 
     *
     * <p>If automatic referral following is disabled and one or more 
@@ -126,24 +130,25 @@ public class LDAPSearchResults implements Enumeration {
     * @exception LDAPException A general exception which includes an error
     *                          message and an LDAP error code.
     */
-   public LDAPEntry next() throws LDAPException {
-      Object element = elements.nextElement();
-	  if( Debug.LDAP_DEBUG ) {
-	  	Debug.trace( Debug.referrals,
-	  		"LDAPSearchResults: next. next object is " +
-	  		element.getClass().getName());
-	  }
-      if(element instanceof LDAPResponse) {
-         ((LDAPResponse)element).chkResultCode(); // will throw an exception
-      }
-      return (LDAPEntry)element;
-   }
+    public LDAPEntry next() throws LDAPException
+    {
+        Object element = elements.nextElement();
+        if( Debug.LDAP_DEBUG ) {
+            Debug.trace( Debug.messages,
+                "LDAPSearchResults: next: found object " +
+                element.getClass().getName());
+        }
+        if(element instanceof LDAPResponse) {
+            ((LDAPResponse)element).chkResultCode(); // will throw an exception
+        }
+        return (LDAPEntry)element;
+    }
 
-   /*
+    /*
     * 4.35.5 nextElement
     */
 
-   /**
+    /**
     * Returns the next result in the enumeration as an Object. 
     *
     * <p>The nextElement method is the default implementation of the
@@ -152,15 +157,22 @@ public class LDAPSearchResults implements Enumeration {
     *
     * @return The next element in the enumeration.
     */
-   public Object nextElement() {
-      return elements.nextElement();
-   }
+    public Object nextElement()
+    {
+        Object element = elements.nextElement();
+        if( Debug.LDAP_DEBUG ) {
+            Debug.trace( Debug.messages,
+                "LDAPSearchResults: nextElement: found object " +
+                element.getClass().getName());
+        }
+        return element;
+    }
 
-   /*
+    /*
     * 4.35.6 sort
     */
 
-   /**
+    /**
     * Sorts all entries in the results using the provided comparison
     * object. 
     *
@@ -186,10 +198,11 @@ public class LDAPSearchResults implements Enumeration {
     *                  interface to compare two objects of type
     *                  LDAPEntry.
     */
-   public void sort(LDAPEntryComparator comp) {
-   }
+    public void sort(LDAPEntryComparator comp) {
+        throw new RuntimeException("LDAPSearchResults: sort not implemented");
+    }
 
-   /**
+    /**
     * @internal
     *
     * Will collect batchSize elements from an LDAPSearchListener message
@@ -200,69 +213,63 @@ public class LDAPSearchResults implements Enumeration {
     * If a null is returned from getResponse(), it is likely that the search
     * was abandoned.
     */
-   private boolean getBatchOfResults() {
-      LDAPMessage msg;
-      for(int i=0; i<batchSize;) {
-         try {
-            if((msg = listener.getResponse()) != null) {
-               controls = msg.getControls();
-               if(msg instanceof LDAPSearchResult) {
-                  entries.addElement(((LDAPSearchResult)msg).getEntry()); // can we optimize this?
-				  if( Debug.LDAP_DEBUG ) {
-				  	Debug.trace( Debug.referrals,
-				  		"LDAPSearchResults: getBatchOfResults: got LDAPSearchResult");
-				  }
-                  count++;
-                  i++;
-               }
-               else if(msg instanceof LDAPSearchResultReference) {
-				  if( Debug.LDAP_DEBUG )
-				  	Debug.trace( Debug.referrals,
-				  		"LDAPSearchResults: getBatchOfResults: got LDAPSearchResultReference");
-               }
-               else { // SearchResultDone
-                  int resultCode = ((LDAPResponse)msg).getResultCode();
-				  if( Debug.LDAP_DEBUG )
-					  Debug.trace( Debug.referrals,
-					  	"LDAPSearchResults: getBatchOfResults: got LDAPSearchResultDone, status " +
-						resultCode);
+    private boolean getBatchOfResults()
+    {
+        LDAPMessage msg;
+        for(int i=0; i<batchSize;) {
+            try {
+                if((msg = listener.getResponse()) != null) {
+                    controls = msg.getControls();
+                    if(msg instanceof LDAPSearchResult) {
+                        entries.addElement(((LDAPSearchResult)msg).getEntry()); // can we optimize this?
+                        if( Debug.LDAP_DEBUG )
+                            Debug.trace( Debug.messages,
+                            "LDAPSearchResults: getBatchOfResults: got LDAPSearchResult");
+                        count++;
+                        i++;
+                    } else 
+                    if(msg instanceof LDAPSearchResultReference) {
+                          if( Debug.LDAP_DEBUG )
+                              Debug.trace( Debug.messages,
+                                  "LDAPSearchResults: getBatchOfResults: got LDAPSearchResultReference");
+                    } else { // SearchResultDone
+                        int resultCode = ((LDAPResponse)msg).getResultCode();
+                        if( Debug.LDAP_DEBUG )
+                            Debug.trace( Debug.messages,
+                            "LDAPSearchResults: getBatchOfResults: got LDAPSearchResultDone, status " +
+                            resultCode);
 
-                  if(resultCode != LDAPException.SUCCESS) {
-                     entries.addElement(msg);
-                  }
-                  return true; // search completed
-               }
+                        if(resultCode != LDAPException.SUCCESS) {
+                            entries.addElement(msg);
+                        }
+                        return true; // search completed
+                    }
+                } else {
+                    // how can we arrive here?
+                    // we would have to have no responses, no message IDs and no
+                    // exceptions
+                    return true;
+                }
+            } catch(LDAPException e) { // network error
+                // could be a client timeout result
+                //          LDAPResponse response = new LDAPResponse(e.getLDAPResultCode());
+                //          entries.addElement(response);
+                return true; // search has been interrupted with an error
             }
-            else {
-               // how can we arrive here?
-               // we would have to have no responses, no message IDs and no
-               // exceptions
+        }
+        return false; // search not completed
+    }
 
-               return true;
-            }
-         }
-         catch(LDAPException e) { // network error
-            // could be a client timeout result
-//          LDAPResponse response = new LDAPResponse(e.getLDAPResultCode());
-//          entries.addElement(response);
-            return true; // search has been interrupted with an error
-         }
-      }
-      return false; // search not completed
-   }
-
-   /**
+    /**
     * Cancels the search request and clears the message and enumeration.
     */
-   public void abandon() {
-      // first, remove message ID and timer and any responses in the queue
-      listener.abandonAll();
+    public void abandon() {
+        // first, remove message ID and timer and any responses in the queue
+        listener.abandonAll();
 
-      // next, clear out enumeration
-      entries.setSize(0);
-      elements = entries.elements();
-      completed = true;
-   }
-
+        // next, clear out enumeration
+        entries.setSize(0);
+        elements = entries.elements();
+        completed = true;
+    }
 }
-
