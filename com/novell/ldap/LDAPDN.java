9f1f03380e6cc6cfb070a5961f5bdac36db9a5a6
cmorris (Novell) <cmorris@novell.openldap.org>
2000-11-09 23:50:31 +0000

    Added methods equal, explodeDN, explodeRDN. Also added private utilities methods equalRDN, explodeLDAPName

--- com/novell/ldap/LDAPDN.java
+++ com/novell/ldap/LDAPDN.java
@@ -14,7 +14,9 @@
  ***************************************************************************/
 
 package com.novell.ldap;
-import java.lang.RuntimeException;                 
+import java.lang.RuntimeException;
+import java.util.StringTokenizer;
+
 
 /**
  *  A utility class to facilitate composition and deomposition
@@ -35,16 +37,28 @@ public class LDAPDN {
     *
     * @return Returns true if the two strings correspond to the same DN; false
     *         if the DNs are different.
+    *
+    * Comments:
+    * Each Attribute name must be escaped.  An attribute type must be in the same
+    * form as the attribute type compared to.  An OID cannot be compared to the name representing it.
     */
+
    public static boolean equals (String dn1, String dn2) {
-      //throw new RuntimeException("Method LDAPDN.equals not implemented");
-      if (dn1.equals(dn2)){ //short-cut, otherwise look up OID's
+      if (dn1.equals(dn2)){ //short-cut, otherwise check thoughly
          return true;
       }
-      else
-      {
-         return false;
+      String[] dnA = LDAPDN.explodeDN(dn1, false); //this will handle white space between RDNs
+      String[] dnB = LDAPDN.explodeDN(dn2, false);
+      if (dnA.length != dnB.length){
+         return false;  //
       }
+
+      for(int i = 0; i < dnA.length; i++){
+         if (!RDNequals(dnA[i], dnB[i])){
+            return false;
+         }
+      }
+      return true;
    }
 
    /**
@@ -70,19 +84,20 @@ public class LDAPDN {
       //throw new RuntimeException("Method LDAPDN.escapeRDN not implemented");
       StringBuffer escapedS = new StringBuffer(rdn);
       int i = 0;
-      
+
       while (i < escapedS.length() && escapedS.charAt(i) != '='){
          i++;  //advance until we find the separator =
       }
       if ( i == escapedS.length()){
          throw new RuntimeException("Attribute type and name must be separated by an equal symbol, '='");
       }
+
       i++;
       //check for a space or # at the beginning of a string.
       if ((escapedS.charAt(i) == ' ') || (escapedS.charAt(i) == '#')){
          escapedS.insert(i++, '\\');
       }
-      
+
       for ( ; i < escapedS.length(); i++){ //loop from second char to the second to last
          char test = escapedS.charAt(i);
          if((escapedS.charAt(i) == ',') || (escapedS.charAt(i) == '+') || (escapedS.charAt(i) == '"') || (escapedS.charAt(i) == '\\')
@@ -121,7 +136,7 @@ public class LDAPDN {
 
       while (i < rdn.length() && rdn.charAt(i) != '='){
          i++;  //advance until we find the separator =
-      }   
+      }
       if ( i == rdn.length()){
          throw new RuntimeException("Attribute type and name must be separated by an equal symbol, '='");
       }
@@ -138,8 +153,8 @@ public class LDAPDN {
                (rdn.charAt(i+1) == '<') || (rdn.charAt(i+1) == '>') || (rdn.charAt(i+1) == ';'))
             {  //I'm not sure if I have to check for these special chars
                continue;
-            } 
-            //check if the last two chars are "\ " 
+            }
+            //check if the last two chars are "\ "
             else if ((rdn.charAt(i+1) == ' ') && (i+2 == rdn.length())){//if the last char is a space
                continue;
             }
@@ -165,12 +180,12 @@ public class LDAPDN {
     * of a DN, or null if the DN is not a valid DN.
     */
    public static String[] explodeDN(String dn, boolean noTypes) {
-      throw new RuntimeException("Method LDAPDN.explodeDN not implemented");
+      return explodeLDAPName(dn, ",", noTypes);
    }
 
    /**
     * Returns the individual components of a relative distinguished name
-    * (RDN).
+    * (RDN), normalized.
     *
     *  @param rdn     The relative distinguished name, or in other words,
     *                 the left-most component of a distinguished name.
@@ -181,10 +196,113 @@ public class LDAPDN {
     *
     * @return An array of strings representing the individual components
     * of an RDN, or null if the RDN is not a valid RDN.
-
     */
    public static String[] explodeRDN(String rdn, boolean noTypes) {
-      throw new RuntimeException("Method LDAPRDN.explodeRDN not implemented");
+      return explodeLDAPName(rdn, "+", noTypes);
    }
 
-}
+
+   /* Returns the individual components of a LDAP name, normalized.  Used by explodeRDN and explodeDN
+    * @param ldapName      Probably RDN or DN.
+    * @param delimiter     "," for DN and "+" for RDN
+    *
+    * Comments:  Parses through and normalizes ldapName.  It ignores the escaped escaped chars.
+    * To be compatibile with LDAP 2, This ignores whitespace to both sides of the Delimiter, equals and +.
+    * The results will not have the extra white space.
+    */
+   private static String[] explodeLDAPName(String ldapName, String delimiter, boolean noTypes){
+      StringTokenizer tok = new StringTokenizer(ldapName, "\\ ,+=" , true);
+      String[] list = new String[tok.countTokens()];  //does this need to be halfed because of the "true"?
+      //Note that because of the slash and space that there will be more tokens than what will be returned.
+      //we will have to shorten that before we return it.
+      String str = "", curToken, temp = "";
+      int strCounter = 0;
+      boolean trimWhiteSpace = true;  //this is set true after a new line, + or =.
+
+
+      while(tok.hasMoreTokens()){
+         curToken = tok.nextToken();
+
+         if (curToken.equals(" ") ){
+            temp = "";
+            while(tok.hasMoreTokens() && curToken.equals(" ")){
+               temp += curToken;    //pass up white_space
+               curToken = tok.nextToken();
+            }
+            if (!tok.hasMoreTokens() && curToken.equals(" ")){
+               curToken = ""; //if this is the last element we don't want the space tacked on.
+            }
+            else if (!curToken.equals(delimiter) && !curToken.equals("+")
+                  && !curToken.equals("=") && !trimWhiteSpace){
+               // Don't trim white space for the following:
+               // 1)!End of line, 2)!End of component 3)!Beginning of component
+               str = str + temp;
+            }
+         }
+         if (noTypes && curToken.equals("=")){ //throw away current string
+            str = "";
+            curToken = "";
+            trimWhiteSpace = true;
+         }
+         else if (curToken.equals("\\") && tok.hasMoreTokens()){//escaped character, skip it.
+            str = str + curToken + tok.nextToken();
+            trimWhiteSpace = false;
+         }
+         else if (curToken.equals(delimiter)){ //end of component, save it
+            list[strCounter++] = str;
+            str = "";
+            trimWhiteSpace = true;
+         }
+         else{
+            str = str + curToken;
+            //normalization flag for whitespace
+            if (curToken.equals("+") || curToken.equals("=")){
+               trimWhiteSpace = true;
+            }else{
+               trimWhiteSpace = false;
+            }
+         }
+      }
+      list[strCounter++] = str;    //save last component
+
+      //so that list.length is accurate we'll copy it into a new array.
+      String array[] = new String[strCounter];
+      for(int i = 0; i < strCounter; i++){
+         array[i]=list[i];
+      }//I'd like to put in a better way to do this.
+      return array;
+   }
+
+
+
+   /*  This utility function is not defined in the API thus it is private.  'equals'
+    * (for Distinguished names) uses this.  It breaks up the RDN multivalued
+    *  attributes and looks to see if every value in one is present in the other RDN.
+    */
+
+   private static boolean RDNequals(String rdn1, String rdn2){
+      if (rdn1.equals(rdn2))
+         return true; //shortcut
+
+      String rdnA[] = explodeRDN( rdn1, false);
+      String rdnB[] = explodeRDN( rdn2, false);
+
+      if (rdnA.length != rdnB.length)
+         return false;
+
+      boolean found = false;
+      for(int i=0; i < rdnA.length; i++){
+         found = false;
+         for(int j=0; j < rdnB.length && !found; j++){
+            if (rdnA[i].equals(rdnB[j])) {
+               found = true;
+            }
+         }
+         if (found == false){  //could not find rdnA[i] in rdnB
+            return false;
+         }
+      }
+      return true;
+   }//end RDNequals
+
+}//end class LDAPDN
