87c18369a867ee1d586483159fc7bda6de7bd238
fzhao (Novell) <fzhao@novell.openldap.org>
2001-12-04 20:52:57 +0000

    Replace it with 'GetBindDNResponse.java'.

--- com/novell/ldap/extensions/GetContextIdentityNameResponse.java
+++ com/novell/ldap/extensions/GetContextIdentityNameResponse.java
@@ -14,76 +14,20 @@
  ******************************************************************************/
 package com.novell.ldap.extensions;
 
-import com.novell.ldap.*;
-import com.novell.ldap.asn1.*;
-import com.novell.ldap.rfc2251.*;
+import com.novell.ldap.LDAPExtendedResponse;
+import com.novell.ldap.LDAPException;
+import com.novell.ldap.asn1.LBERDecoder;
+import com.novell.ldap.asn1.ASN1OctetString;
+import com.novell.ldap.rfc2251.RfcLDAPMessage;
 import java.io.IOException;
 
 /**
- *  Retrieves the identity from an GetContextIdentityNameResponse object.
- *
- *  <p>An object in this class is generated from an LDAPExtendedResponse object
- *  using the ExtendedResponseFactory class.</p>
- *
- * <p>The GetContextIdentityNameResponse extension uses the following OID:<br>
- *  &nbsp;&nbsp;&nbsp;2.16.840.1.113719.1.27.100.32 </p>
- *
+ *  deprecated. @See GetBindDNResponse.
  */
-public class GetContextIdentityNameResponse extends LDAPExtendedResponse {
-
-   // Identity returned by the server
-   private String identity;
-
-   /**
-    * Constructs an object from the responseValue which contains the identity.
-    *
-    *  <p>The constructor parses the responseValue which has the following
-    *  format:<br>
-    *  responseValue ::=<br>
-    *  &nbsp;&nbsp;&nbsp;&nbsp;identity &nbsp;&nbsp;&nbsp;  OCTET STRING</p>
-    *
-    *  @exception IOException The return value could not be decoded.
-    */
-   public GetContextIdentityNameResponse (RfcLDAPMessage rfcMessage)
+public class GetContextIdentityNameResponse extends LDAPExtendedResponse {  
+    public GetContextIdentityNameResponse (RfcLDAPMessage rfcMessage)
          throws IOException {
 
         super(rfcMessage);
-        
-        if (getResultCode() == LDAPException.SUCCESS)
-        {
-            // parse the contents of the reply
-            byte [] returnedValue = this.getValue();
-            if (returnedValue == null)
-                throw new IOException("No returned value");
-
-            // Create a decoder object
-            LBERDecoder decoder = new LBERDecoder();
-            if (decoder == null)
-                throw new IOException("Decoding error");
-
-            // The only parameter returned should be an octet string
-            ASN1OctetString asn1_identity = (ASN1OctetString)decoder.decode(returnedValue);
-            if (asn1_identity == null)
-                throw new IOException("Decoding error");
-
-            // Convert to normal string object
-            identity = new String(asn1_identity.getContent());
-            if (identity == null)
-                throw new IOException("Decoding error");
-        }
-        else
-        {
-            identity = "";
-        }
    }
-
-   /**
-    * Returns the identity of the object.
-    *
-    * @return A string value specifying the identity returned by the server.
-    */
-   public String getIdentity() {
-        return identity;
-   }
-
 }
