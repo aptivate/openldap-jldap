1064fe23d744ae83e3ed95ac8055df0bbea4af6a
fzhao (Novell) <fzhao@novell.openldap.org>
2001-12-04 18:40:47 +0000

    Replace it with 'RemoveOrphanPartitionRequest.java'

--- com/novell/ldap/extensions/RemoveOrphanNamingContextRequest.java
+++ com/novell/ldap/extensions/RemoveOrphanNamingContextRequest.java
@@ -8,75 +8,21 @@
  * TO VERSION 2.0.1 OF THE OPENLDAP PUBLIC LICENSE, A COPY OF WHICH IS
  * AVAILABLE AT HTTP://WWW.OPENLDAP.ORG/LICENSE.HTML OR IN THE FILE "LICENSE"
  * IN THE TOP-LEVEL DIRECTORY OF THE DISTRIBUTION. ANY USE OR EXPLOITATION
- * OF THIS WORK OTHER THAN AS AUTHORIZED IN VERSION 2.0.7 OF THE OPENLDAP
+ * OF THIS WORK OTHER THAN AS AUTHORIZED IN VERSION 2.0.1 OF THE OPENLDAP
  * PUBLIC LICENSE, OR OTHER PRIOR WRITTEN CONSENT FROM NOVELL, COULD SUBJECT
  * THE PERPETRATOR TO CRIMINAL AND CIVIL LIABILITY.
  ******************************************************************************/
 package com.novell.ldap.extensions;
 
-import com.novell.ldap.*;
-import com.novell.ldap.asn1.*;
-import com.novell.ldap.resources.*;
-import java.io.IOException;
-import java.io.ByteArrayOutputStream;
+import com.novell.ldap.LDAPException;
 
 /**
- *  Deletes an orphan naming context (NDS partition).
- *
- *  <p>To delete an orphan partition, you must create an instance of this
- *  class and then call the extendedOperation method with this
- *  object as the required LDAPExtendedOperation parameter.</p>
- *
- *  <p>The removeOrphanNamingContextRequest extension uses the following OID:<br>
- *  &nbsp;&nbsp;&nbsp;2.16.840.1.113719.1.27.100.41</p>
- *
- *  <p>The requestValue has the following format:<br>
- *
- *  requestValue ::=<br>
- * &nbsp;&nbsp;&nbsp;&nbsp;      serverDN &nbsp;&nbsp;&nbsp;    LDAPDN<br>
- * &nbsp;&nbsp;&nbsp;&nbsp;      contextName &nbsp;&nbsp;&nbsp; LDAPDN</p>
+ * deprecated. @see RemoveOrphanPartitionRequest.
  */
-public class RemoveOrphanNamingContextRequest extends LDAPExtendedOperation {
+public class RemoveOrphanNamingContextRequest extends RemoveOrphanPartitionRequest {
+    public RemoveOrphanNamingContextRequest(String serverDN, String contextName)
+                                                          throws LDAPException {
 
-/**
- * Constructs an extended operation object for deleting an orphan naming context.
- *
- * @param serverDN   The distinguished name of the server
- *                   on which the orphan naming context resides.
- *<br><br>
- * @param contextName  The distinguished name of the orphan
- *                     naming context to delete.
- *
- * @exception LDAPException A general exception which includes an error message
- *                          and an LDAP error code.
- */
- public RemoveOrphanNamingContextRequest(String serverDN, String contextName)
-                throws LDAPException {
-
-        super(NamingContextConstants.REMOVE_ORPHAN_NAMING_CONTEXT_REQ, null);
-
-        try {
-
-            if ( (serverDN == null) || (contextName == null) )
-                throw new LDAPException(ExceptionMessages.PARAM_ERROR,
-                                    LDAPException.PARAM_ERROR);
-
-            ByteArrayOutputStream encodedData = new ByteArrayOutputStream();
-         LBEREncoder encoder  = new LBEREncoder();
-
-          ASN1OctetString asn1_serverDN = new ASN1OctetString(serverDN);
-          ASN1OctetString asn1_contextName = new ASN1OctetString(contextName);
-
-            asn1_serverDN.encode(encoder, encodedData);
-            asn1_contextName.encode(encoder, encodedData);
-
-            setValue(encodedData.toByteArray());
-
-        }
-      catch(IOException ioe) {
-         throw new LDAPException(ExceptionMessages.ENCODING_ERROR,
-                                 LDAPException.ENCODING_ERROR);
-      }
+        super(serverDN, contextName);
    }
-
 }
