045344c215a3336be3d04450a9106b074bf14fb4
fzhao (Novell) <fzhao@novell.openldap.org>
2001-12-04 18:29:57 +0000

    Replace it with SplitPartitionRequest.java

--- com/novell/ldap/extensions/CreateNamingContextRequest.java
+++ com/novell/ldap/extensions/CreateNamingContextRequest.java
@@ -14,73 +14,15 @@
  ******************************************************************************/
 package com.novell.ldap.extensions;
 
-import com.novell.ldap.*;
-import com.novell.ldap.asn1.*;
-import com.novell.ldap.resources.*;
-import java.io.IOException;
-import java.io.ByteArrayOutputStream;
+import com.novell.ldap.LDAPException;
 
 /**
- *
- *  Creates a new naming context (or in NDS terminology, a new partition).
- *
- *  <p>To create a new naming context, you must create an instance of this
- *  class and then call the extendedOperation method with this
- *  object as the required LDAPExtendedOperation parameter.</p>
- *
- *  <p>The createNamingContextRequest extension uses the following OID:<br>
- *  &nbsp;&nbsp;&nbsp;2.16.840.1.113719.1.27.100.3</p>
- *
- *  <p>The requestValue has the following format:<br>
- *
- *  requestValue ::=<br>
- *  &nbsp;&nbsp;&nbsp;&nbsp; flags &nbsp;&nbsp;&nbsp;&nbsp; INTEGER<br>
- *  &nbsp;&nbsp;&nbsp;&nbsp; dn    &nbsp;&nbsp;&nbsp;&nbsp; LDAPDN</p>
+ *  deprecated. @see SplitPartitionRequest.
  */
-public class CreateNamingContextRequest extends LDAPExtendedOperation {
-
-/**
- *
- *  Constructs an extended operation object for creating a naming context.
- *
- * @param dn      The distinguished name of the container where the new naming
- *                context root should be located.
- *<br><br>
- * @param flags Specifies whether all servers in the replica ring must be up before
- *              proceeding. When set to zero, the status of the servers is not
- *              checked. When set to LDAP_ENSURE_SERVERS_UP, all servers must be up
- *              for the operation to proceed.
- *
- * @exception LDAPException A general exception which includes an error message
- *                          and an LDAP error code.
- */
- public CreateNamingContextRequest(String dn, int flags)
+public class CreateNamingContextRequest extends SplitPartitionRequest {
+    public CreateNamingContextRequest(String dn, int flags)
                 throws LDAPException {
 
-        super(NamingContextConstants.CREATE_NAMING_CONTEXT_REQ, null);
-
-        try {
-
-            if (dn == null)
-                throw new LDAPException(ExceptionMessages.PARAM_ERROR,
-                                    LDAPException.PARAM_ERROR);
-
-            ByteArrayOutputStream encodedData = new ByteArrayOutputStream();
-         LBEREncoder encoder  = new LBEREncoder();
-
-          ASN1Integer asn1_flags = new ASN1Integer(flags);
-          ASN1OctetString asn1_dn = new ASN1OctetString(dn);
-
-            asn1_flags.encode(encoder, encodedData);
-            asn1_dn.encode(encoder, encodedData);
-
-            setValue(encodedData.toByteArray());
-
-        }
-      catch(IOException ioe) {
-         throw new LDAPException(ExceptionMessages.ENCODING_ERROR,
-                                 LDAPException.ENCODING_ERROR);
-      }
-   }
-
+        super(dn, flags);
+    }
 }
