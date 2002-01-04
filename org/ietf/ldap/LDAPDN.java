05f9349af45000245ef31d20a7e753749e3b8125
Steve Sonntag <vtag@openldap.org>
2002-01-04 01:12:35 +0000

    Change javadoc @see links for splitup of ietf and novell javadoc

--- org/ietf/ldap/LDAPDN.java
+++ org/ietf/ldap/LDAPDN.java
@@ -19,16 +19,24 @@ package org.ietf.ldap;
  *  A utility class to facilitate composition and deomposition
  *  of distinguished names (DNs).
  *
- * @see com.novell.ldap.LDAPDN
+ * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html">
+            com.novell.ldap.LDAPDN</a>
  */
 public class LDAPDN
 {
+    // Don't need instances of this utility class
+    protected LDAPDN()
+    {
+        return;
+    }
 
    /**
     * Compares the two strings per the distinguishedNameMatch equality matching
     * (using case-ignore matching).
     *
-    * @see com.novell.ldap.LDAPDN#equals(String, String)
+    * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html
+            #equals(java.lang.String, java.lang.String)">
+            com.novell.ldap.LDAPDN.equals(String, String)</a>
     */
 
     public static boolean equals (String dn1, String dn2)
@@ -39,7 +47,9 @@ public class LDAPDN
     /**
      * Returns the RDN after escaping the characters requiring escaping.
      *
-     * @see com.novell.ldap.LDAPDN#escapeRDN(String)
+     * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html
+            #escapeRDN(java.lang.String)">
+            com.novell.ldap.LDAPDN.escapeRDN(String)</a>
      */
     public static String escapeRDN (String rdn)
     {
@@ -49,7 +59,9 @@ public class LDAPDN
     /**
      * Returns the individual components of a distinguished name (DN).
      *
-     * @see com.novell.ldap.LDAPDN#explodeDN(String,boolean)
+     * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html
+            #explodeDN(java.lang.String, boolean)">
+            com.novell.ldap.LDAPDN.explodeDN(String, boolean)</a>
      */
     public static String[] explodeDN(String dn, boolean noTypes)
     {
@@ -59,7 +71,9 @@ public class LDAPDN
     /**
      * Returns the individual components of a relative distinguished name
      *
-     * @see com.novell.ldap.LDAPDN#explodeRDN(String,boolean)
+     * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html
+            #explodeRDN(java.lang.String, boolean)">
+            com.novell.ldap.LDAPDN.explodeRDN(String, boolean)</a>
      */
     public static String[] explodeRDN(String rdn, boolean noTypes) {
         return com.novell.ldap.LDAPDN.explodeRDN(rdn,noTypes);
@@ -68,7 +82,9 @@ public class LDAPDN
     /**
      * Returns true if the string conforms to distinguished name syntax.
      *
-     * @see com.novell.ldap.LDAPDN#isValid(String)
+     * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html
+            #isValid(java.lang.String)">
+            com.novell.ldap.LDAPDN.isValid(String)</a>
      */
     public static boolean isValid(String dn){
         return com.novell.ldap.LDAPDN.isValid(dn);
@@ -77,7 +93,9 @@ public class LDAPDN
     /**
      * Returns the DN normalized by removal of non-significant space characters.
      *
-     * @see com.novell.ldap.LDAPDN#normalize(String)
+     * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html
+            #normalize(java.lang.String)">
+            com.novell.ldap.LDAPDN.normalize(String)</a>
      */
     public static String normalize(String dn){
         return com.novell.ldap.LDAPDN.normalize(dn);
@@ -87,7 +105,9 @@ public class LDAPDN
     /**
      * Returns the RDN after unescaping the characters requiring escaping.
      *
-     * @see com.novell.ldap.LDAPDN#unescapeRDN(String)
+     * @see <a href="../../../../doc/com/novell/ldap/LDAPDN.html
+            #unescapeRDN(java.lang.String)">
+            com.novell.ldap.LDAPDN.unescapeRDN(String)</a>
      */
     public static String unescapeRDN (String rdn) {
         return com.novell.ldap.LDAPDN.unescapeRDN(rdn);
