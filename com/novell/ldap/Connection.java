aefd37ce171bfdea2038263db206e8c07b45dd5a
Sunil Kumar <sunilk@openldap.org>
2004-05-03 08:50:22 +0000

    updated version number to 4.0.

--- com/novell/ldap/Connection.java
+++ com/novell/ldap/Connection.java
@@ -125,7 +125,7 @@ final class Connection
     // these to be local variables that can be modified using
     // the setProperty method.
     /* package */
-    static String sdk = new String("3.5");
+    static String sdk = new String("4.0");
     /* package */
     static Integer protocol = new Integer(3);
     /* package */
