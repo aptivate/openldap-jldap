01da83f241aef081e6cbfaade4c2fa042b73544a
Jon Roberts <mentata@openldap.org>
2005-11-23 21:37:38 +0000

    Add Spanish Result Code messages to resources.

---
+++ com/novell/ldap/resources/ResultCodeMessages_es.java
@@ -0,0 +1,73 @@
+package com.novell.ldap.resources;
+
+/**
+ * This class contains Spanish strings corresponding to LDAP Result Codes.
+ * The resources are accessed by the String representation of the result code.
+ */
+
+public class ResultCodeMessages_es extends java.util.ListResourceBundle {
+  public Object[][] getContents() {
+      return contents;
+  }
+  static final Object[][] contents = {
+      {"0", "Éxito"},
+      {"1", "Error De Las Operaciones"},
+      {"2", "Error Del Protocolo"},
+      {"3", "Timelimit Se Excedió"},
+      {"4", "Sizelimit Se Excedió"},
+      {"5", "Comparación Falso"},
+      {"6", "Comparación Verdad"},
+      {"7", "Método De La Autentificación No Apoyado"},
+      {"8", "La Autentificación Fuerte Requirió"},
+      {"9", "Resultados Parciales"},
+      {"10", "Remisión"},
+      {"11", "El Límite Administrativo Se Excedió"},
+      {"12", "Extensión Crítica Inasequible"},
+      {"13", "El Secreto Requirió"},
+      {"14", "Lazo De SASL En Marcha"},
+      {"16", "Ningún Tal Atributo"},
+      {"17", "Tipo Indefinido Del Atributo"},
+      {"18", "El Emparejar Inadecuado"},
+      {"19", "Violación Del Constreñimiento"},
+      {"20", "El Atributo O El Valor Existe"},
+      {"21", "Sintaxis Inválido Del Atributo"},
+      {"32", "Ningún Tal Objeto"},
+      {"33", "Alias Problema"},
+      {"34", "Sintaxis Inválido Del DN"},
+      {"35", "Es Una Hoja"},
+      {"36", "Alias Problema De Referencia"},
+      {"48", "Autentificación Inadecuada"},
+      {"49", "Credenciales Inválidas"},
+      {"50", "Las Derechas De Acceso Escasas"},
+      {"51", "Ocupado"},
+      {"52", "Inasequible"},
+      {"53", "Poco Dispuesto Realizarse"},
+      {"54", "El Lazo Detectó"},
+      {"64", "Nombramiento De La Violación"},
+      {"65", "Violación De La Clase Del Objeto"},
+      {"66", "No permitido En In-Hoja"},
+      {"67", "No permitido En RDN"},
+      {"68", "La Entrada Existe Ya"},
+      {"69", "Las Modificaciones De La Clase Del Objeto Prohibieron"},
+      {"71", "Afecta DSAs Múltiple"},
+      {"80", "Otro"},
+      {"81", "Servidor Abajo"},
+      {"82", "Error Local"},
+      {"83", "Error De Codificación"},
+      {"84", "Error El Descifrar"},
+      {"85", "Descanso De LDAP"},
+      {"86", "Desconocido De La Autentificación"},
+      {"87", "Error Del Filtro"},
+      {"88", "El Usuario Canceló"},
+      {"89", "Error Del Parámetro"},
+      {"90", "Ninguna Memoria"},
+      {"91", "Conecte El Error"},
+      {"92", "LDAP No Apoyado"},
+      {"93", "Controle No Encontrado"},
+      {"94", "Ningunos Resultados Volvieron"},
+      {"95", "Más Resultados A Volver"},
+      {"96", "Lazo Del Cliente"},
+      {"97", "El Límite De la Remisión Se Excedió"},
+      {"112", "TLS No Apoyado"}
+  };
+}//End ResultCodeMessages
